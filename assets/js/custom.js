6028caa7bd521ae9a9c4b822a290ae4bcd9a64f5
Ashwin Goel <ashwin.goel2017@vitstudent.ac.in>
2018-11-06 00:38:02 +0530

    Final changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -288,3 +288,25 @@ $(function () {
     }
 
 });
+
+
+
+//-------------------------------------------------------------------------------
+
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#camcannVideo").attr('src');
+
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function(){
+        $("#camcannVideo").attr('src', '');
+    });
+
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal").on('show.bs.modal', function(){
+        $("#camcannVideo").attr('src', url);
+    });
+});
